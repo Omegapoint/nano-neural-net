378e562db116f5190d8ce0143c9c6382ac104a90
Jonathan Ström <jonathan.strom@omegapoint.se>
2024-03-14 15:24:07 +0100

    Moved all graphviz stuff
    Like the title says; better to keep all graphviz stuff in the graphviz file.
    
    Also added the ability to provide a name to the output files.

--- src/graphviz.rs
+++ src/graphviz.rs
@@ -1,4 +1,5 @@
 use crate::node::{Node, Operation}; // Adjust the import path according to your project structure
+use std::process::Command;
 
 impl Operation {
     pub fn as_str(&self) -> &'static str {
@@ -7,6 +8,7 @@ impl Operation {
             Operation::Subtract => "-",
             Operation::Multiply => "*",
             Operation::Divide => "/",
+            Operation::Tanh => "tanh",
         }
     }
 }
@@ -54,4 +56,21 @@ pub fn to_dot<'a>(root: &Node<'a>) -> String {
     generate_dot(root, &mut dot, &mut node_counter);
     dot.push_str("}");
     dot
+}
+
+pub fn render_dot(dot_content: String, output_name: String) {
+    // Use output_name to create the DOT file name
+    let dot_file_name = format!("{}.dot", output_name);
+    std::fs::write(&dot_file_name, dot_content).expect("Unable to write DOT file");
+
+    // Use output_name to create the PNG file name
+    let png_file_name = format!("{}.png", output_name);
+    
+    // Run the Graphviz `dot` command to generate the PNG from the DOT file
+    Command::new("dot")
+        .args(["-Tpng", &dot_file_name, "-o", &png_file_name])
+        .output()
+        .expect("Failed to execute Graphviz dot command");
+
+    println!("Generated {} from {}.", png_file_name, dot_file_name);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod graphviz;
 mod node;
-use std::process::Command;
 
-use crate::graphviz::to_dot;
+
+use crate::graphviz::{to_dot, render_dot};
 use crate::node::Node;
 
-fn generate_and_render_dot() {
+
+
+fn generate_simple_network() {
     let a = Node::new_with_label(3.0, "a".to_string());
     let b = Node::new_with_label(4.0, "b".to_string());
     let mut c = &a + &b;
@@ -16,18 +18,46 @@ fn generate_and_render_dot() {
     let mut e = &c * &d;
     e.set_label("e".to_string());
 
-    let dot_content = to_dot(&e);
+    let f = Node::new_with_label(2.0, "f".to_string());
 
-    std::fs::write("output.dot", dot_content).expect("Unable to write DOT file");
+    let mut L = &e / &f;
+    L.set_label("L".to_string());
 
-    Command::new("dot")
-        .args(["-Tpng", "output.dot", "-o", "output.png"])
-        .output()
-        .expect("Failed to execute Graphviz dot command");
+    let dot_content = to_dot(&L);
 
-    println!("Generated output.png from output.dot.");
+    render_dot(dot_content, "simple".to_string());
+}
+
+fn generate_neuron() {
+    let w1 = Node::new_with_label(1.0, "w1".to_string());
+    let w2 = Node::new_with_label(2.0, "w2".to_string());
+
+    let x1 = Node::new_with_label(3.0, "x1".to_string());
+    let x2 = Node::new_with_label(4.0, "x2".to_string());
+
+    let b = Node::new_with_label(1.0, "b".to_string());
+
+    let mut x1w1 = &w1 * &x1;
+    x1w1.set_label("x1*w1".to_string());
+
+    let mut x2w2 = &w2 * &x2;
+    x2w2.set_label("x2*w2".to_string());
+    let mut x1w1x2w2 = &x1w1 + &x2w2;
+    x1w1x2w2.set_label("x1*w1 + x2*w2".to_string());
+
+    let mut n = &x1w1x2w2 + &b;
+    n.set_label("n".to_string());
+
+    let mut o = n.tanh();
+    o.set_label("o".to_string());
+
+
+    let dot_content = to_dot(&o);
+
+    render_dot(dot_content, "neuron".to_string());
 }
 
 fn main() {
-    generate_and_render_dot();
+    generate_simple_network();
+    generate_neuron();
 }
